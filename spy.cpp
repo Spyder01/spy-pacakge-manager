9bb82a67b547f802cf12b735040b6e556c7fac2f
Spyder01 <suhan01.bangera@gmail.com>
2021-07-09 00:17:31 +0530

    Registering Packages in JSON

--- spy.cpp
+++ spy.cpp
@@ -4,13 +4,17 @@
 #include <unistd.h>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <string>
 #include <ctype.h>
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
+#include "nlohmann/json.hpp"
+
 using namespace std;
+using json = nlohmann::json;
 
 
+
+//Structure to store initialiazation details.
  struct Details {
    string author;
    string project;
@@ -21,13 +25,14 @@ using namespace std;
 
 
 
-
+//Package Manager Class
 class Packager {
 public:
    char** args;
    int argN;
    string command;
    fstream spyJSON;
+   json JSON;
 
    Packager (int ArgN, char** Args) {
      args = Args;
@@ -37,11 +42,14 @@ public:
    }
    void checker();
    void init();
+   bool exists(const string name);
    void cloner();
+   void ClonePusher(char* Identity);
 
 };
 
 
+//Function to keppa check on the commands.
 void Packager::checker () {
   if(command=="init")
     init();
@@ -50,6 +58,8 @@ void Packager::checker () {
 }
 
 
+
+//Function to initiaze the project.
 void Packager::init () {
   int flag = 0;
   string answer = "no";
@@ -76,15 +86,26 @@ void Packager::init () {
     else flag=0;
  }
 
-
- spyJSON.open("spy-package.json", ios::out | ios::in);
-  string entry = "\n{\n\t\"name\":" +details.project+",\n\t\"description\":" +details.description+",\n\t\"version\":" +details.version+",\n\t\"author\":" +details.author+",\n}\n";
-  //cout<<entry;
+  ofstream JSONFile;
+  JSONFile.open("spy-package.json");
+  JSON["author"] = details.author;
+  JSON["version"] = details.version;
+  JSON["description"] = details.description;
+  JSON["name"] = details.project;
+  JSON["dependencies"]["cloned"] =  json::array();
+  JSON["dependencies"]["installed"] = json::array();
+  JSONFile<<JSON.dump(4);
+  JSONFile.close();
 }
 
+bool Packager::exists (const string name) {
+    return ( access( name.c_str(), F_OK ) != -1 );
+}
+
+//Function to add outside cloned repositories.
 void Packager::cloner () {
-
-
+if(exists("spy-package.json")){
+  char* Identity = args[2];
   mkdir("spy-Dependencies", 70006);
   chdir("spy-Dependencies");
   mkdir("cloned",70007);
@@ -93,11 +114,32 @@ void Packager::cloner () {
   string package = "git clone https://github.com/"+string(args[2])+".git";
   char Command[package.length()+1];
   strcpy(Command, package.c_str());
+try {
   system(Command);
 }
+ catch(...) {
+   exit(0);
+ }
+ ClonePusher(Identity);
 
+ }
+  else cerr<<"\n ERROR: \"Initiaze Your Project First\"\n";
+}
 
-
+//Function to register the cloned repository to spy-package.json
+void Packager::ClonePusher(char* Identity) {
+  chdir("..");
+  chdir("..");
+  ifstream i("spy-package.json");
+  i >> JSON;
+  cout<<JSON.dump(4)<<endl;
+  JSON["dependencies"]["cloned"].push_back(Identity);
+  ofstream spyJSON;
+  cout<<JSON.dump(4)<<endl;
+  spyJSON.open("spy-package.json");
+  spyJSON<<JSON.dump(4);
+  spyJSON.close();
+}
 
 
 
